133b475e6eb30d9f4c8fbe13edf2b6ec6e23df16
Ian Marshall <imarshall14@gmail.com>
2020-12-18 08:30:31 -0500

    commit tree 76ec09f64bfd7269995d9e08ca3813111ea22338

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,13 @@
+require "pry"
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    BRANDS.include?(brand) ? BRANDS : BRANDS << brand
   end
 
   def cobble
@@ -11,4 +15,7 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+  def brand=(brand)
+    @brand = brand
+  end
 end
